f1dec85cf626660f54be9dc85429aff112d81f92
Ricardo <rycardo.191@gmail.com>
2024-12-07 03:44:39 -0300

    Model Task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Task extends Model
 {
     use HasFactory;
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+    
+    protected $fillable = ['title', 'description', 'status', 'user_id'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($task) {
+            $task->id = (string) Str::uuid();
+        });
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }

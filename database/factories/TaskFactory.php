b9f73526ac97821b5f78a04bd6cfeeab4be6a6a4
Ricardo <rycardo.191@gmail.com>
2024-12-07 03:46:15 -0300

    Factories para Task

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+    protected $model = Task::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->word,
+            'description' => $this->faker->sentence,
+            'status' => $this->faker->randomElement(['pending', 'completed'])
+        ];
+    }
+}

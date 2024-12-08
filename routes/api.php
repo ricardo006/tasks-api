ca3d7a0424a3eb16b522f98e4d7bfa750281bca6
Ricardo <rycardo.191@gmail.com>
2024-12-08 17:14:02 -0300

    Rotas da API

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
